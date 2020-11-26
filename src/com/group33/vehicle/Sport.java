04beee83d6dfeb3402bd556ffaf30c0c502109d3
Aghoghomena <aghoghojao@gmail.com>
2020-11-26 22:27:46 +0000

    Added vehicle and drivers class skeleton

--- src/com/group33/vehicle/Sport.java
+++ src/com/group33/vehicle/Sport.java
@@ -1,22 +1,6 @@
 package com.group33.vehicle;
 
-import com.group33.road.RoadDirection;
 
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
+public class Sport {
 
-public class Sport extends Vehicle{
-    public Sport(int newXAxis, int newYAxis, File newVehicleImageFilePath, RoadDirection newVehicleDirection) {
-        super(newXAxis, newYAxis, newVehicleImageFilePath, newVehicleDirection);
-        this.setVehicleSpeed(60);
-        this.setVehicleHeight(50);
-        this.setVehicleWidth(90);
-        try {
-            this.vehicleImage = ImageIO.read(this.vehicleImageFilePath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        this.vehicleImage = vehicleImage.getScaledInstance(this.vehicleWidth, this.getVehicleHeight(), vehicleImage.SCALE_DEFAULT);
-    }
 }
