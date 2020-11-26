dcd854a2742c77a4eb8e79b344a4b49f7a4bea94
Nnadozie Omeonu <nnadozieome@gmail.com>
2020-11-26 23:21:16 +0000

    implemented bulb class to light class and identified some properties on the left and right road

--- src/com/group33/road/RightRoad.java
+++ src/com/group33/road/RightRoad.java
@@ -1,6 +1,11 @@
 package com.group33.road;
 
-
-
 public class RightRoad {
+
+    protected int roadWidth;
+    protected final int roadHeight = 600;
+    protected int laneHeight;
+    protected int numberOfLane;
+    protected int xAxis;
+    protected int yAxis;
 }
