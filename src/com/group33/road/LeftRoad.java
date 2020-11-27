0414081544b7de5b0161a7d6f57cd1f774041aac
Achilles <achilles@Achilless-MacBook-Pro.local>
2020-11-27 00:05:37 +0000

    add configrations

--- src/com/group33/road/LeftRoad.java
+++ src/com/group33/road/LeftRoad.java
@@ -9,4 +9,58 @@ public class LeftRoad {
     protected int xAxis;
     protected int yAxis;
 
+    public LeftRoad(int roadWidth, int laneHeight, int numberOfLane, int xAxis, int yAxis) {
+        this.roadWidth = roadWidth;
+        this.laneHeight = laneHeight;
+        this.numberOfLane = numberOfLane;
+        this.xAxis = xAxis;
+        this.yAxis = yAxis;
+    }
+
+    public int getRoadWidth() {
+        return roadWidth;
+    }
+
+    public void setRoadWidth(int roadWidth) {
+        this.roadWidth = roadWidth;
+    }
+
+    public int getRoadHeight() {
+        return roadHeight;
+    }
+
+    public int getLaneHeight() {
+        return laneHeight;
+    }
+
+    public void setLaneHeight(int laneHeight) {
+        this.laneHeight = laneHeight;
+    }
+
+    public int getNumberOfLane() {
+        return numberOfLane;
+    }
+
+    public void setNumberOfLane(int numberOfLane) {
+        this.numberOfLane = numberOfLane;
+    }
+
+    public int getxAxis() {
+        return xAxis;
+    }
+
+    public void setxAxis(int xAxis) {
+        this.xAxis = xAxis;
+    }
+
+    public int getyAxis() {
+        return yAxis;
+    }
+
+    public void setyAxis(int yAxis) {
+        this.yAxis = yAxis;
+    }
+
+ 
+
 }
