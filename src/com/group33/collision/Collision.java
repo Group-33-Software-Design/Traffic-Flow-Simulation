30ff871574d89018133709b1623c7080c0733c59
Achilles <achilles@Achilless-MacBook-Pro.local>
2020-12-13 23:32:13 +0000

    implement collision detection

--- src/com/group33/collision/Collision.java
+++ src/com/group33/collision/Collision.java
@@ -10,12 +10,34 @@ public class Collision {
     public static ArrayList<Vehicle> vehicles;
 
     /**
-     * @todo we will implement the collision check method
+     * implement collision 
      * @return
      */
-    public static boolean collision(){
-        return false;
+    public static boolean collision(int x, int y, Vehicle vehicle, RoadDirection vehicleDirection){
+        boolean collide = false;
+        //loop through all the vehicles
+        for(int a = 0; a < vehicles.size(); a++){
+            Vehicle u = vehicles.get(a);
+            if(y == u.getyAxis()){ // If I'm in the same lane
+                if(u.equals(vehicle) == false)// if i'm not checking myself
+                {
+                    if(RoadDirection.RIGHT == vehicleDirection) {
+                        if (x < u.getxAxis() + u.getVehicleWidth() && // my left side is left of his right
+                                x + vehicle.getVehicleWidth() > u.getxAxis()) {//my right side is right of his left
+                            return true;
+                        }
+                    }
+                    else{
+                        if (x < u.getxAxis() + u.getVehicleWidth() && // my left side is left of his right
+                                x + vehicle.getVehicleWidth() > u.getxAxis()) {//my right side is right of his left
+                            return true;
+                        }
 
+                    }
+                }
+            }
+        }
+        return false;
     }
 
 
