28d01558bd17b0684d7ee9fde1623f2f7afb9e37
Aghoghomena <aghoghojao@gmail.com>
2020-12-13 22:28:32 +0000

    Update the LeftRoad and RightRoad to extend the Road class. Added the RoadFactory implementation and a getRoad function in the Road Factory

--- src/com/group33/road/RoadFactory.java
+++ src/com/group33/road/RoadFactory.java
@@ -4,6 +4,27 @@ import static com.group33.road.RoadDirection.*;
 
 public class RoadFactory {
 
+    private int xAxis, yAxis, roadWidth, numberOfLane, laneHeight;
+    private RoadDirection newVehicleDirection;
+
+    public RoadFactory(int xAxis, int yAxis, int roadWidth,  int numberOfLane, int laneHeight, RoadDirection newVehicleDirection) {
+        this.xAxis = xAxis;
+        this.yAxis = yAxis;
+        this.roadWidth = roadWidth;
+        this.numberOfLane = numberOfLane;
+        this.laneHeight = laneHeight;
+        this.newVehicleDirection = newVehicleDirection;
+    }
+
+    public Road getRoad(){
+        switch(this.newVehicleDirection){
+            case RIGHT:
+                return new RightRoad(xAxis, yAxis, roadWidth, numberOfLane, laneHeight, RIGHT);
+            case LEFT:
+                return new LeftRoad(xAxis, yAxis, roadWidth, numberOfLane, laneHeight, LEFT);
+        }
+        return null;
+    }
 
 
 }
