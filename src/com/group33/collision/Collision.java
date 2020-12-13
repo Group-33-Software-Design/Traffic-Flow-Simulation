652e20050ee1fa21ff52a1fc27a176a4ed6c9e8e
“ifedeji” <ifebimpeadedeji@gmail.com>
2020-12-13 23:16:54 +0000

    Created Collision class and empty methods

---
+++ src/com/group33/collision/Collision.java
@@ -0,0 +1,32 @@
+package com.group33.collision;
+
+import com.group33.road.RoadDirection;
+import com.group33.vehicle.Vehicle;
+
+import java.util.ArrayList;
+
+public class Collision {
+
+    public static ArrayList<Vehicle> vehicles;
+
+    /**
+     * @todo we will implement the collision check method
+     * @return
+     */
+    public static boolean collision(){
+        return false;
+
+    }
+
+
+
+
+
+    public static ArrayList<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public static void setVehicles(ArrayList<Vehicle> vehicles) {
+        Collision.vehicles = vehicles;
+    }
+}
